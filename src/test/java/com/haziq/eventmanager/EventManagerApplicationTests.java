0c362981b081774269555c9aa362e78cb8823dad
Haziq <haziqridzwan@outlook.com>
2026-07-11 12:18:23 +0800

    Spring Boot project setup

---
+++ src/test/java/com/haziq/eventmanager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haziq.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
